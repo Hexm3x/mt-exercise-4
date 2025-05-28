c1d182e3eeedf0dc5a41523dfc5799cd8e7c3e65
medine.balci <medine.balci@uzh.ch>
2025-05-28 16:28:13 +0200

    creating sh file for bpe 2000

---
+++ scripts/evaluate_bpe_2000.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+scripts=$(dirname "$0")
+base=$scripts/..
+
+data=$base/nl-de
+configs=$base/configs
+
+translations=$base/translations
+
+src=nl
+trg=de
+
+num_threads=4
+device=0
+
+# BPE 2000
+# measure time
+
+SECONDS=0
+
+VOCAB_SIZE=2004
+model_name2=model_name_BPE_2000
+
+echo "###############################################################################"
+echo "model_name2 $model_name2"
+
+translations_sub=$translations/$model_name2
+mkdir -p $translations_sub
+
+# translate using JoeyNMT
+CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name2.yaml < $data/test_100k.$src > $translations_sub/test.$model_name2.$trg
+
+# trim target to match for fair BLEU
+head -n ${VOCAB_SIZE} $data/test_100k.$trg > $data/cut_${VOCAB_SIZE}.$trg
+
+# compute BLEU score
+cat $translations_sub/test.$model_name2.$trg | sacrebleu $data/cut_${VOCAB_SIZE}.$trg | tee $translations_sub/bleu.txt
+
+echo "Done evaluating BPE model with vocab size $VOCAB_SIZE."
+
+echo "time taken:"
+echo "$SECONDS seconds"
+
